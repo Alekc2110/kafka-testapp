c44d29205c366c40d4a97162d9cdc52b891160b3
Aleksandr Korshunovych <korsunovica@gmail.com>
2024-03-01 01:40:25 +0200

    create bat scripts to start-stop zookeeper, kafka

---
+++ data-producer/kafka-start.bat
@@ -0,0 +1,3 @@
+start C:\Kafka\kafka\bin\windows\zookeeper-server-start.bat C:\Kafka\kafka\config\zookeeper.properties
+timeout 30
+start C:\Kafka\kafka\bin\windows\kafka-server-start.bat C:\Kafka\kafka\config\server.properties
---
+++ data-producer/kafka-stop.bat
@@ -0,0 +1,3 @@
+start C:\Kafka\kafka\bin\windows\zookeeper-server-stop.bat
+timeout 10
+start C:\Kafka\kafka\bin\windows\kafka-server-stop.bat
